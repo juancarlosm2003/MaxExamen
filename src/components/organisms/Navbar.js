1c9cfa4aa00335e94f14d91e5bbc896816bcaa9f
h <101147415+juancarlosm2003@users.noreply.github.com>
2025-06-02 14:51:56 -0600

    Examen definitivo del Examen

--- src/components/organisms/Navbar.js
+++ src/components/organisms/Navbar.js
@@ -1,24 +1,69 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import './Navbar.css';
 import { FaSearch, FaRegBookmark, FaUserCircle } from 'react-icons/fa';
 
 function Navbar() {
+  const [isHovering, setIsHovering] = useState(false);
+  const timeoutRef = useRef(null);
+
+  const handleMouseEnter = () => {
+    clearTimeout(timeoutRef.current);
+    setIsHovering(true);
+  };
+
+  const handleMouseLeave = () => {
+    timeoutRef.current = setTimeout(() => {
+      setIsHovering(false);
+    }, 300); // 300 ms de retraso
+  };
+
   return (
     <nav className="navbar">
-      <div className="logo-nav">
-        <h1 className="logo">max</h1>
-        <ul className="nav-links">
-          <li className="active">Inicio</li>
-          <li>Series</li>
-          <li>Películas</li>
-          <li>HBO</li>
-          <li>Niños y Familia</li>
-        </ul>
+      <div className="navbar-left">
+        <a href="/">
+          <img src="/MAX-rebrand-logo.png" alt="Max logo" className="logo" />
+        </a>
       </div>
+
+
+      <ul className="nav-links">
+        <li className="active">Inicio</li>
+        <li>Series</li>
+        <li>Películas</li>
+        <li>
+          <a href="/" title="HBO">
+            <img src="\HBO.png" alt="HBO logo" className="logo" />
+          </a>
+        </li>
+        <li>Niños y Familia</li>
+      </ul>
+
       <div className="navbar-right">
         <FaSearch className="icon" />
         <FaRegBookmark className="icon" />
-        <FaUserCircle className="icon" />
+        <div
+          className="user-icon"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <FaUserCircle className="icon" />
+          {isHovering && (
+            <div
+              className="user-popup"
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              <div className="popup-section user-header">Perfil 1</div>
+              <div className="popup-section">Administrar perfiles</div>
+              <div className="popup-section">Ajustes</div>
+              <div className="popup-section">Cuenta</div>
+              <div className="popup-section">Suscripción</div>
+              <div className="popup-section">Privacidad</div>
+              <div className="popup-section">Ayuda</div>
+              <div className="popup-section logout">Cerrar sesión</div>
+            </div>
+          )}
+        </div>
       </div>
     </nav>
   );
--- src/components/organisms/Top10Section.js
+++ src/components/organisms/Top10Section.js
@@ -1,56 +1,67 @@
 import React from 'react';
 import './Top10Section.css';
+import { link } from 'framer-motion/client';
 
 const top10Data = [
   {
     id: 1,
     title: 'The Last of Us',
     image: 'https://m.media-amazon.com/images/M/MV5BYWI3ODJlMzktY2U5NC00ZjdlLWE1MGItNWQxZDk3NWNjN2RhXkEyXkFqcGc@._V1_.jpg',
+    link: 'https://play.max.com/show/93ba22b1-833e-47ba-ae94-8ee7b9eefa9a'  
   },
   {
     id: 2,
     title: 'Love & Death',
     image: 'https://m.media-amazon.com/images/M/MV5BYjkwYzU3MWItMzkwMi00MmZhLWEzMzAtZDcwN2MwYTU0NTRiXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg',
+    link: 'https://play.max.com/show/f7b16534-5a7b-4e0e-906f-406d25e6b7af'
   },
   {
     id: 3,
     title: 'Hacks',
     image: 'https://m.media-amazon.com/images/M/MV5BMGI5NTE0ODMtOTdlYS00MjY1LWE0ZWUtZTg2M2FmZmUwZGMwXkEyXkFqcGc@._V1_.jpg',
+    link: 'https://play.max.com/show/67e940b7-aab2-46ce-a62b-c7308cde9de7'
   },
   {
     id: 4,
     title: 'Miedo Profundo',
     image: 'https://m.media-amazon.com/images/M/MV5BM2MyMDk2NjMtNTFlYy00ZmFhLTkwZmEtNWMwNzFmMDhlYzcyXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg',
+    link: 'https://play.max.com/movie/eff0e361-c3b3-4a94-a618-df079cc7be07'
   },
   {
     id: 5,
     title: 'Found',
     image: 'https://m.media-amazon.com/images/M/MV5BOGVmOTg0ZWEtMWIwNS00NjkwLThiYWMtOWJmNGFkZWJhYzVhXkEyXkFqcGc@._V1_.jpg',
+    link: 'https://play.max.com/show/a83f2f0a-1a91-4374-99f2-c218d6fcef27'
   },
   {
     id: 6,
     title: 'Euphoria',
     image: 'https://m.media-amazon.com/images/M/MV5BZjVlN2M2N2MtOWViZC00MzIxLTlhZWEtMTIwNDIwMzE3ZWJiXkEyXkFqcGc@._V1_.jpg',
+    link: 'https://play.max.com/show/4ffd33c9-e0d6-4cd6-bd13-34c266c79be0'
   },
   {
     id: 7,
     title: 'Succession',
     image: 'https://m.media-amazon.com/images/M/MV5BNTEwNTFkZTktMzI1OC00YmRjLWE5NTUtYmZhMmJlNGUxMWU1XkEyXkFqcGc@._V1_.jpg',
+    link: 'https://play.max.com/show/a8484031-f244-4661-9fb7-0932bd1ba872'
   },
   {
     id: 8,
     title: 'Barry',
     image: 'https://m.media-amazon.com/images/M/MV5BYzdlYWZkNjQtMWYwNi00YjNkLTljYjgtZjRhMmQ2YTQ1MWQ0XkEyXkFqcGc@._V1_.jpg',
+    link: 'https://play.max.com/show/179bdb1c-83f8-4ab7-87ef-47ce3b566a13'
   },
   {
     id: 9,
     title: 'Chernobyl',
     image: 'https://m.media-amazon.com/images/M/MV5BNzU0OTI4YTQtNGQ1ZS00ZjA4LTg3MTMtZjkyZWNjN2RiZDJmXkEyXkFqcGc@._V1_.jpg',
+    link: 'https://play.max.com/mini-series/396999a6-3fff-4af3-802b-10c46d10deff'
   },
   {
     id: 10,
     title: 'Game of Thrones',
     image: 'https://m.media-amazon.com/images/M/MV5BMTNhMDJmNmYtNDQ5OS00ODdlLWE0ZDAtZTgyYTIwNDY3OTU3XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg',
+    link: 'https://play.max.com/show/4f6b4985-2dc9-4ab6-ac79-d60f0860b0ac'
   }
 ];
 
@@ -59,7 +70,7 @@ function Top10Section() {
     <div className="top10-section">
       <div className="top10-title-container">
         <h2>
-          TOP 10 <span>PELÍCULAS</span>
+          TOP 10 <span>SERIES</span>
         </h2>
         <span>HOY</span>
       </div>
@@ -67,7 +78,9 @@ function Top10Section() {
         {top10Data.map((item, index) => (
           <div className="top10-card" key={item.id}>
             <div className="top10-number">{index + 1}</div>
-            <img src={item.image} alt={item.title} />
+            <a href={item.link} target="_blank" rel="noopener noreferrer">
+              <img src={item.image} alt={item.title} style={{ cursor: 'pointer' }} />
+            </a>
           </div>
         ))}
       </div>
@@ -76,3 +89,4 @@ function Top10Section() {
 }
 
 export default Top10Section;
+
